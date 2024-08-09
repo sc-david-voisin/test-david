ea8c9223cef95498f08d1008e65948e32083850e
David Voisin <david.voisin@scalingo.com>
2024-08-09 13:56:26 +0200

    dynamically configure HSTS header

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
-header("Strict-Transport-Security: max-age=123");
+$age = $_GET["age"];
+
+if(isset($age)) {
+	header("Strict-Transport-Security: max-age=".$age);
+}
+
+print_r($_GET);
 
 echo "<h1>Born to be alive ^^</h1>";
 
